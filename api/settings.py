ba6375f1ace43efb8a647b1b16f50d9690d280dc
Víctor Alfredo García Calvillo <132796314+victorgarcia2006@users.noreply.github.com>
2025-10-06 15:58:47 -0600

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -134,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "rc-ed-production.up.railway.app"
+    "rc-ed-production.up.railway.app/",
 ]
